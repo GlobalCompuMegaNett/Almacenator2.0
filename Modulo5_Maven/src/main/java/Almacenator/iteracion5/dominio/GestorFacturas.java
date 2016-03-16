8555e142854ca7252f5fb2d585782cdaa8221d55
GlobalCompuMegaNett <josemanuelplana.global@gmail.com>
2016-03-16 10:56:37 +0100

    Modulo5_Maven

---
+++ Modulo5_Maven/src/main/java/Almacenator/iteracion5/dominio/GestorFacturas.java
@@ -0,0 +1,10 @@
+package Almacenator.iteracion5.dominio;
+
+
+public class GestorFacturas {
+	
+	public static void ModificarPedido(String nombre,String ref) throws ClassNotFoundException{
+		FacturaDao.ModificarPedidos(nombre,ref);
+	}
+	
+}
