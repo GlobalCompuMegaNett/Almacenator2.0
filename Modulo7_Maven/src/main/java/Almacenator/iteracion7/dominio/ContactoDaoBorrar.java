6b40063386d0796ce512b8e77a1dcb92693a830a
GlobalCompuMegaNett <josemanuelplana.global@gmail.com>
2016-03-22 19:03:57 +0100

    Modulo7_Maven

---
+++ Modulo7_Maven/src/main/java/Almacenator/iteracion7/dominio/ContactoDaoBorrar.java
@@ -0,0 +1,18 @@
+package Almacenator.iteracion7.dominio;
+
+import java.io.IOException;
+
+import Almacenator.iteracion2.persistencia.Agente;
+
+public class ContactoDaoBorrar {
+	
+	public static void BorrarCliente(String nombre) {
+		try {
+			Agente.BorrarCliente(nombre);
+		} catch (IOException e) {
+			System.out.println("no se ha podido guardar el contacto ");
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ Modulo7_Maven/src/main/java/Almacenator/iteracion7/dominio/GestorBorrar.java
@@ -0,0 +1,12 @@
+package Almacenator.iteracion7.dominio;
+
+import java.io.IOException;
+
+public class GestorBorrar {
+
+	public static void BorrarCliente(String nombre) throws IOException {
+		ContactoDaoBorrar.BorrarCliente(nombre);
+	}
+
+}
+
